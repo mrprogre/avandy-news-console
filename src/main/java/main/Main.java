e2cfde86eeaf109a149576e5c7f7f8d1594aab08
Dmitry <rps_project@mail.ru>
2023-11-17 22:56:16 +0300

    ref

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
         //System.out.println("Command example: from@mail.ru from_password to@mail.ru 1440 first second thirdKeyword");
 
         // test
-        String[] argsTest = {"rps_project@mail.ru", "FrcviJrX3Yjg0qrwdbeu", "rps_project@mail.ru", "30", "а", "б"};
+        String[] argsTest = {"rps_project@mail.ru", "", "rps_project@mail.ru", "30", "а", "б"};
         args = argsTest;
 
         if (args.length != 0) {
