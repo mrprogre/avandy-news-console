93750462baf0e613ba15f0f641ed4afcc5fb202d
Dmitry <rps_project@mail.ru>
2023-11-17 23:12:01 +0300

    ref

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,9 +8,8 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Command example: from@mail.ru from_password to@mail.ru 1440 first second thirdKeyword");
 
-        // test
-        String[] argsTest = {"rps_project@mail.ru", Properties.EMAIL_PASSWORD, "rps_project@mail.ru", "30", "а", "б"};
-        args = argsTest;
+        // test --> Need to create class "Properties" with String EMAIL_PASSWORD = "password"
+        String[] argsTest = {"rps_project@mail.ru", Properties.EMAIL_PASSWORD, "rps_project@mail.ru", "30", "а", "б"}; args = argsTest;
 
         if (args.length != 0) {
             new ConsoleSearch().searchByConsole(args);
